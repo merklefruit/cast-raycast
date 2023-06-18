2e244ea9147c5f94e2b5586a9e0cdad0858eb47d
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-06-18 10:38:46 +0200

    feat: added all block commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import * as Abi from "./abi";
 import * as Account from "./account";
+import * as Block from "./block";
 import * as Ens from "./ens";
 import * as Utility from "./utility";
 import * as Wallet from "./wallet";
@@ -9,6 +10,7 @@ import { CommandCategory } from "./types";
 const allCommands: CommandCategory[] = [
   { title: "ABI", items: Abi },
   { title: "Account", items: Account },
+  { title: "Block", items: Block },
   { title: "ENS", items: Ens },
   { title: "Utility", items: Utility },
   { title: "Wallet", items: Wallet },
